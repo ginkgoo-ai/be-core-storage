78a74a702a6a15d7f1cafc68d9d0576da0b1a49e
seasonsolt <seasonsolt@gmail.com>
2025-07-14 09:45:34 +0800

    fix: update security configuration to allow all requests from internal call

--- be-core-storage-app/src/main/java/com/ginkgooai/config/security/SecurityConfig.java
+++ be-core-storage-app/src/main/java/com/ginkgooai/config/security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/health"
                         ).permitAll()
-                        .anyRequest().authenticated()
+				.anyRequest()
+				.permitAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
